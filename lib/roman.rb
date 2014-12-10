e1c70ca70399cef2e179443681abb66d6df23040
Ricardo Mendes <rokusu@gmail.com>
2014-12-10 23:36:51 +0000

    potatoes

--- lib/roman.rb
+++ lib/roman.rb
@@ -14,35 +14,15 @@ class Roman
     case arabic
     when 0..3 then "I" * arabic
     when 4 then "IV"
-    when 5..8 then five_to_eight(arabic)
+    when 5..8 then lalala("V", 5, arabic)
     when 9 then "IX"
-    when 40...50 then fourty_range(arabic)
-    when 50...60 then fifty_range(arabic)
-    when 90...100 then ninety_range(arabic)
-    when ->(x) { x >= 10 } then ten_range(arabic)
+    when 10...40 then lalala("X", 10, arabic)
+    when 40...50 then lalala("XL", 40, arabic)
+    when 50...90 then lalala("L", 50, arabic)
+    when 90...100 then lalala("XC", 90, arabic)
     else
       raise BadInputError
     end
-  end
-
-  def five_to_eight arabic
-    lalala "V", 5, arabic
-  end
-
-  def ten_range arabic
-    lalala "X", 10, arabic
-  end
-
-  def fourty_range arabic
-    lalala "XL", 40, arabic
-  end
-
-  def fifty_range arabic
-    lalala "L", 50, arabic
-  end
-
-  def ninety_range arabic
-    lalala "XC", 90, arabic
   end
 
   def lalala roman_base, arabic_base, arabic
