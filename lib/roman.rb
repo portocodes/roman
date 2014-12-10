057e9f748f17c1de87966444c691a4ae49867975
Ricardo Mendes <rokusu@gmail.com>
2014-12-10 23:31:46 +0000

    Partir tudo

--- lib/roman.rb
+++ lib/roman.rb
@@ -4,12 +4,44 @@ class BadInputError < StandardError; end
 
 class Roman
   def convert arabic_numeral
-    case arabic_numeral
-    when 1..3 then "I" * arabic_numeral
+    raise BadInputError if arabic_numeral.zero?
+
+    actually_converts arabic_numeral
+  end
+
+  private
+  def actually_converts arabic
+    case arabic
+    when 0..3 then "I" * arabic
     when 4 then "IV"
-    when 5 then "V"
+    when 5..8 then five_to_eight(arabic)
+    when 9 then "IX"
+    when 40...50 then fourty_range(arabic)
+    when 50...60 then fifty_range(arabic)
+    when 90...100 then ninety_range(arabic)
+    when ->(x) { x >= 10 } then ten_range(arabic)
     else
       raise BadInputError
     end
+  end
+
+  def five_to_eight arabic
+    "V" << actually_converts(arabic - 5)
+  end
+
+  def ten_range arabic
+    "X" << actually_converts(arabic - 10)
+  end
+
+  def fourty_range arabic
+    "XL" << actually_converts(arabic - 40)
+  end
+
+  def fifty_range arabic
+    "L" << actually_converts(arabic - 50)
+  end
+
+  def ninety_range arabic
+    "XC" << actually_converts(arabic - 90)
   end
 end
--- test/roman_test.rb
+++ test/roman_test.rb
@@ -42,4 +42,82 @@ class RomanTest < Minitest::Test
     assert_equal "V", roman
   end
 
+  def test_converts_6
+    roman = convert_number 6
+
+    assert_equal "VI", roman
+  end
+
+  def test_converts_7
+    roman = convert_number 7
+
+    assert_equal "VII", roman
+  end
+
+  def test_converts_8
+    roman = convert_number 8
+
+    assert_equal "VIII", roman
+  end
+
+  def test_converts_9
+    roman = convert_number 9
+
+    assert_equal "IX", roman
+  end
+
+  def test_converts_10
+    roman = convert_number 10
+
+    assert_equal "X", roman
+  end
+
+  def test_converts_10_range
+    {11 => "XI", 12 => "XII", 14 => "XIV", 15 => "XV", 19 => "XIX"}.each do |arabic, roman|
+      assert_equal roman, convert_number(arabic)
+    end
+  end
+
+  def test_converts_20
+    roman = convert_number 20
+
+    assert_equal "XX", roman
+  end
+
+  def test_converts_40
+    roman = convert_number 40
+
+    assert_equal "XL", roman
+  end
+
+  def test_converts_40_range
+    {41 => "XLI", 42 => "XLII", 44 => "XLIV", 45 => "XLV", 49 => "XLIX"}.each do |arabic, roman|
+      assert_equal roman, convert_number(arabic)
+    end
+  end
+
+  def test_converts_50
+    roman = convert_number 50
+
+    assert_equal "L", roman
+  end
+
+  def test_converts_50_range
+    {51 => "LI", 52 => "LII", 54 => "LIV", 55 => "LV", 59 => "LIX"}.each do |arabic, roman|
+      assert_equal roman, convert_number(arabic)
+    end
+  end
+
+  def test_converts_90
+    roman = convert_number 90
+
+    assert_equal "XC", roman
+  end
+
+  def test_converts_90_range
+    {91 => "XCI", 92 => "XCII", 94 => "XCIV", 95 => "XCV", 99 => "XCIX"}.each do |arabic, roman|
+      assert_equal roman, convert_number(arabic)
+    end
+  end
+
 end
